e5cd6b36d33ab7278f2d0af9ad61792cf69febe8
winwu <yiyingwu.1990@gmail.com>
2016-08-04 20:28:13 +0800

    init project

---
+++ Accounting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Accounting.Startup))]
+namespace Accounting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
